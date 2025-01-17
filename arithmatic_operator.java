7abdfa02adc6e37979362fd1258fd5f4331ddf93
Idba1 <moniraislamself@gmail.com>
2025-01-17 10:31:14 +0600

    Arithmetic Operators

---
+++ arithmatic_operator.java
@@ -0,0 +1,11 @@
+public class arithmatic_operator {
+    public static void main(String[] args) {
+        System.out.println(7 + 9);
+        System.out.println(17 - 9);
+        System.out.println(6 * 7);
+        System.out.println(7.0 / 2);
+        System.out.println(7 / 2.0);
+        System.out.println(7 / 2);
+        System.out.println(7 % 2);
+    }
+}
