169f57fb94aa5506524a7f1d429418be02a56a27
Idba1 <moniraislamself@gmail.com>
2025-01-17 10:42:23 +0600

    Logical Operators

---
+++ assignment_opperation.java
@@ -0,0 +1,21 @@
+public class assignment_opperation {
+    public static void main(String[] args) {
+        // System.out.println();
+        int num1 = 78;
+        // num1 = num1 + 4;
+        num1 += 4;
+        System.out.println(num1);
+        num1 -= 2;
+        System.out.println(num1);
+        num1 /= 4;
+        System.out.println(num1);
+        num1 *= 4;
+        System.out.println(num1);
+        System.out.println(6 > 8);
+        System.out.println(6 < 8);
+        System.out.println(7 == 7);
+        System.out.println(7 != 7);
+        System.out.println((5 == 9) || (2 == 2));
+        System.out.println((5 == 9) && (2 == 2));
+    }
+}
