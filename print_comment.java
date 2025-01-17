3778dcb3023fdb1f3fd7f4e54b29e000a5438487
Idba1 <moniraislamself@gmail.com>
2025-01-17 10:24:51 +0600

    Java Print Statements and Comments

---
+++ print_comment.java
@@ -0,0 +1,11 @@
+public class print_comment {
+    public static void main(String[] args) {
+        System.out.println("hello Idba!");
+        System.out.print("Hello");
+        System.out.print(" ");
+        System.out.print("Idba 2 \n");
+        // comment
+        double value = 54.644334;
+        System.out.printf("%.2f", value);
+    }
+}
