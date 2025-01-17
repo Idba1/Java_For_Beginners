17dc32f9a5e0c8b0189f88a7e1599a0de5825cdb
Idba1 <moniraislamself@gmail.com>
2025-01-18 03:19:05 +0600

    How to Create and Use Method

---
+++ method.java
@@ -0,0 +1,20 @@
+public class method {
+
+    /*
+     * access_type static return_type/void method_name(parameters){
+     * all code for completing method work;
+     * return;
+     * }
+     */
+
+    // add method
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        System.out.println(add(7, 9));
+    }
+}
